2e6eda581ffa51ac10f84fc73f7ac43fc471244c
harsilspatel <harsilspatel@gmail.com>
2019-05-09 18:33:09 +1000

    Add trip info VC and assets

---
+++ bon-voyage/TripInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TripInfoViewController.swift
+//  bon-voyage
+//
+//  Created by Harsil Patel on 9/5/19.
+//  Copyright © 2019 Harsil Patel. All rights reserved.
+//
+
+import UIKit
+
+class TripInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
