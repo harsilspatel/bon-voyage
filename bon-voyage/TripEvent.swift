c3dd74c7d291a100c1e4047a08be7346ce323628
harsilspatel <harsilspatel@gmail.com>
2019-06-14 04:25:56 +1000

    refactor Event to TripEvent

--- bon-voyage/TripEvent.swift
+++ bon-voyage/TripEvent.swift
@@ -1,5 +1,5 @@
 //
-//  Event.swift
+//  TripEvent.swift
 //  bon-voyage
 //
 //  Created by Harsil Patel on 14/6/19.
@@ -9,41 +9,44 @@
 import Firebase
 import Foundation
 
-class Event: NSObject {
-    var title: String
-    var start: Date
-    var duration: Int
+class TripEvent: NSObject {
+    let title: String
+    let start: Date
+    let duration: Int
+    let color: UIColor
     
     
     init(title: String, start: Date, duration: Int) {
         self.title = title
         self.start = start
         self.duration = duration
+        self.color = [UIColor.blue,
+                      UIColor.yellow,
+                      UIColor.green,
+                      UIColor.red].randomElement()!
     }
     
-    init?(document: QueryDocumentSnapshot) {
+    convenience init?(document: QueryDocumentSnapshot) {
         let data = document.data()
         
         guard let title = data["title"] as? String else {
-          return nil
+            return nil
         }
         
         guard let timestamp = (data as AnyObject).value(forKey: "start") as? Timestamp else {
-          return nil
+            return nil
         }
         let start = timestamp.dateValue()
         
         guard let duration = data["duration"] as? Int else {
-          return nil
+            return nil
         }
         
-        self.title = title
-        self.start = start
-        self.duration = duration
+        self.init(title: title, start: start, duration: duration)
     }
 }
 
-extension Event: DatabaseRepresentation {
+extension TripEvent: DatabaseRepresentation {
     
     var representation: [String : Any] {
         let rep: [String: Any] = [
