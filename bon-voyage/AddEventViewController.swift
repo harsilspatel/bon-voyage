6c233de0a9e1afd68d495ea9115a17192ad8182d
harsilspatel <harsilspatel@gmail.com>
2019-06-15 18:31:28 +1000

    add validation for new events

--- bon-voyage/AddEventViewController.swift
+++ bon-voyage/AddEventViewController.swift
@@ -11,8 +11,65 @@ import GooglePlaces
 
 class AddEventViewController: UIViewController, GMSAutocompleteViewControllerDelegate {
     
+    // Trip will be the city user wishes to travel
     var trip: Trip?
-    var temp_place: GMSPlace?
+    
+    // The destination selected in the city
+    var selectedPlace: GMSPlace?
+    
+    var addEventDelegate: AddEventDelegate?
+    var placesClient = AppCommons.sharedInstance.placesClient
+
+    // outlets to get data from the view controller
+    @IBOutlet weak var eventTitle: UITextField!
+    @IBOutlet weak var destination: UITextField!
+    @IBOutlet weak var duration: UITextField!
+    @IBOutlet weak var startDate: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // Will be executed when the Destination field is pressed
+    @IBAction func launchPlaceLookup(_ sender: Any) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        
+        // We want the name, placeID and the coordinates of the place
+        let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+            UInt(GMSPlaceField.placeID.rawValue) | UInt(GMSPlaceField.coordinate.rawValue))!
+        autocompleteController.placeFields = fields
+        
+        // We want the user to be go anywhere in the city, therefore .noFilter
+        let filter = GMSAutocompleteFilter()
+        filter.type = .noFilter
+        autocompleteController.autocompleteFilter = filter
+
+
+        // We assume the city to be 1 latitude long and 1 latitude wide centered at the city's coordinates
+        // therefore we bound the searchable area to give results of establishments from only that city.
+        let lat = trip!.tripLat!
+        let lon = trip!.tripLon!
+        let northeEastBound = CLLocationCoordinate2D(latitude: lat - 0.5, longitude: lon - 0.5)
+        let southWestBound = CLLocationCoordinate2D(latitude: lat + 0.5, longitude: lon + 0.5)
+        let bounds = GMSCoordinateBounds(coordinate: northeEastBound, coordinate: southWestBound)
+        autocompleteController.autocompleteBounds = bounds
+        
+        // We can use BoundsMode .bias to give prediction preference to establishments in the bounds over those
+        // that are out of bounds, however, by using .restrict we restrict results to only the ones that are in the bounds
+        autocompleteController.autocompleteBoundsMode = .restrict
+        
+        // Display the autocomplete view controller.
+        present(autocompleteController, animated: true, completion: nil)
+    }
+    
+    // We update the selectedPlace and the destination field's text to chosen place.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        selectedPlace = place
+        destination.text = place.name!
+        dismiss(animated: true, completion: nil)
+    }
+    
     func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
         print("Error: ", error.localizedDescription)
     }
@@ -21,77 +78,36 @@ class AddEventViewController: UIViewController, GMSAutocompleteViewControllerDel
         dismiss(animated: true, completion: nil)
     }
     
-
-    var placesClient: GMSPlacesClient!
-    
-    var addEventDelegate: AddEventDelegate?
-    @IBOutlet weak var eventTitle: UITextField!
-    @IBOutlet weak var destination: UITextField!
-    @IBOutlet weak var duration: UITextField!
-    @IBOutlet weak var startDate: UIDatePicker!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        placesClient = GMSPlacesClient.shared()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func launchPlaceLookup(_ sender: Any) {
-        let autocompleteController = GMSAutocompleteViewController()
-        autocompleteController.delegate = self
-        
-        // Specify the place data types to return.
-        let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
-            UInt(GMSPlaceField.placeID.rawValue) | UInt(GMSPlaceField.coordinate.rawValue))!
-        autocompleteController.placeFields = fields
-        
-        // Specify a filter.
-        let filter = GMSAutocompleteFilter()
-        filter.type = .noFilter
-        //        autocompleteController.auto
-        let lat = trip!.coordinates!.latitude
-        let lon = trip!.coordinates!.longitude
-        let northeEastBound = CLLocationCoordinate2D(latitude: lat - 0.5, longitude: lon - 0.5)
-        let southWestBound = CLLocationCoordinate2D(latitude: lat + 0.5, longitude: lon + 0.5)
-        let bounds = GMSCoordinateBounds(coordinate: northeEastBound, coordinate: southWestBound)
-        autocompleteController.autocompleteFilter = filter
-        autocompleteController.autocompleteBounds = bounds
-        autocompleteController.autocompleteBoundsMode = .restrict
-        //        autocompleteController.autocompleteBounds = .
-        
-        // Display the autocomplete view controller.
-        present(autocompleteController, animated: true, completion: nil)
-    }
-    
-    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-        //        print(place.addressComponents)
-        print(place.coordinate)
-        print(place.name!)
-        print(place.placeID)
-        temp_place = place
-        destination.text = place.name!
-        dismiss(animated: true, completion: nil)
-    }
-    
     @IBAction func addEvent(_ sender: Any) {
-        let title = self.eventTitle.text!
-        let duration = Int(self.duration.text!)!
+        var message = ""
+        var isEventValid = true
         
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-//        let start = dateFormatter.string(from: self.startDate.date)
-        let newEvent = TripEvent(title: title, start: self.startDate.date, duration: duration, place: temp_place!, databaseId: nil)
-        _ = self.addEventDelegate!.addEvent(newEvent: newEvent)
-        navigationController?.popViewController(animated: true)
+        if (self.eventTitle.text?.isEmpty)!  {
+            message += "\nPlease enter the title"
+            isEventValid = false
+        }
+        if (Int(self.duration.text!) == nil)  {
+            message += "\nEnsure duration is integer"
+            isEventValid = false
+        }
+        if (selectedPlace == nil) {
+            message += "\nPlease choose a destination"
+            isEventValid = false
+        }
+    
+        
+        if (isEventValid) {
+            let title = self.eventTitle.text!
+            let duration = Int(self.duration.text!)!
+            
+            let newEvent = TripEvent(title: title, start: self.startDate.date, duration: duration, place: selectedPlace!, databaseId: nil)
+            _ = self.addEventDelegate!.addEvent(newEvent: newEvent)
+            navigationController?.popViewController(animated: true)
+        } else {
+            let alertController = UIAlertController(title: "Please verify the details", message: message, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
