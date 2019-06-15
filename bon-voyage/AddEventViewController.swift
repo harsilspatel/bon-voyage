36dac260ef2f945a5f31a122bb4bebd6795a25c7
harsilspatel <harsilspatel@gmail.com>
2019-06-15 12:22:35 +1000

    add support for locations

--- bon-voyage/AddEventViewController.swift
+++ bon-voyage/AddEventViewController.swift
@@ -7,20 +7,71 @@
 //
 
 import UIKit
+import GooglePlaces
 
-class AddEventViewController: UIViewController {
+class AddEventViewController: UIViewController, GMSAutocompleteViewControllerDelegate {
+    
+    var temp_place: GMSPlace?
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("Error: ", error.localizedDescription)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
+    var placesClient: GMSPlacesClient!
+    
     var addEventDelegate: AddEventDelegate?
     @IBOutlet weak var eventTitle: UITextField!
+    @IBOutlet weak var destination: UITextField!
     @IBOutlet weak var duration: UITextField!
     @IBOutlet weak var startDate: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        placesClient = GMSPlacesClient.shared()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func launchPlaceLookup(_ sender: Any) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        
+        // Specify the place data types to return.
+        let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+            UInt(GMSPlaceField.placeID.rawValue) | UInt(GMSPlaceField.coordinate.rawValue))!
+        autocompleteController.placeFields = fields
+        
+        // Specify a filter.
+        let filter = GMSAutocompleteFilter()
+        filter.type = .address
+        //        autocompleteController.auto
+        let lat = 40.7127753
+        let lon = -74.0059728
+        let northeEastBound = CLLocationCoordinate2D(latitude: lat - 0.5, longitude: lon - 0.5)
+        let southWestBound = CLLocationCoordinate2D(latitude: lat + 0.5, longitude: lon + 0.5)
+        let bounds = GMSCoordinateBounds(coordinate: northeEastBound, coordinate: southWestBound)
+        autocompleteController.autocompleteFilter = filter
+        autocompleteController.autocompleteBounds = bounds
+        autocompleteController.autocompleteBoundsMode = .restrict
+        //        autocompleteController.autocompleteBounds = .
+        
+        // Display the autocomplete view controller.
+        present(autocompleteController, animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        //        print(place.addressComponents)
+        print(place.coordinate)
+        print(place.name!)
+        print(place.placeID)
+        temp_place = place
+        destination.text = place.name!
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func addEvent(_ sender: Any) {
         let title = self.eventTitle.text!
         let duration = Int(self.duration.text!)!
@@ -28,7 +79,7 @@ class AddEventViewController: UIViewController {
 //        let dateFormatter = DateFormatter()
 //        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
 //        let start = dateFormatter.string(from: self.startDate.date)
-        let newEvent = TripEvent(title: title, start: self.startDate.date, duration: duration)
+        let newEvent = TripEvent(title: title, start: self.startDate.date, duration: duration, place: temp_place!, databaseId: nil)
         _ = self.addEventDelegate!.addEvent(newEvent: newEvent)
         navigationController?.popViewController(animated: true)
     }
--- bon-voyage/TripEvent.swift
+++ bon-voyage/TripEvent.swift
@@ -8,15 +8,22 @@
 
 import Firebase
 import Foundation
+import GooglePlaces
 
 class TripEvent: NSObject {
     let title: String
     let start: Date
     let duration: Int
     let color: UIColor
+    let databaseId: String?
+    
+    let placeID: String
+    let placeName: String
+    let placeLat: Double
+    let placeLon: Double
     
     
-    init(title: String, start: Date, duration: Int) {
+    init(title: String, start: Date, duration: Int, placeID: String, placeLat: Double, placeLon: Double, placeName: String, databaseId: String?) {
         self.title = title
         self.start = start
         self.duration = duration
@@ -24,6 +31,22 @@ class TripEvent: NSObject {
                       UIColor.yellow,
                       UIColor.green,
                       UIColor.red].randomElement()!
+        self.databaseId = databaseId
+        
+        self.placeID = placeID
+        self.placeLat = placeLat
+        self.placeLon = placeLon
+        self.placeName = placeName
+    }
+    
+    convenience init(title: String, start: Date, duration: Int, place: GMSPlace, databaseId: String?) {
+        
+        let placeName = place.name!
+        let placeID = place.placeID!
+        let placeLat = place.coordinate.latitude
+        let placeLon = place.coordinate.longitude
+        
+        self.init(title: title, start: start, duration: duration, placeID: placeID, placeLat: placeLat, placeLon: placeLon, placeName: placeName, databaseId: nil)
     }
     
     convenience init?(document: QueryDocumentSnapshot) {
@@ -42,7 +65,35 @@ class TripEvent: NSObject {
             return nil
         }
         
-        self.init(title: title, start: start, duration: duration)
+        guard let placeName = data["placeName"] as? String else {
+            return nil
+        }
+        
+        guard let placeID = data["placeID"] as? String else {
+            return nil
+        }
+        
+        guard let placeLat = data["placeLat"] as? Double else {
+            return nil
+        }
+        
+        guard let placeLon = data["placeLon"] as? Double else {
+            return nil
+        }
+        
+        self.init(title: title, start: start, duration: duration, placeID: placeID, placeLat: placeLat, placeLon: placeLon, placeName: placeName, databaseId: document.documentID)
+    }
+    
+    // to get the index
+    
+    // lesson learnt: if the class is inheriting from NSObject,
+    // you have to have isEqual method as opposed to conforming to Equatable
+    // as NSObject by default conforms to that.
+    override func isEqual(_ object: Any?) -> Bool {
+        if let object = object as? TripEvent {
+            return self.databaseId == object.databaseId
+        }
+        return false
     }
 }
 
@@ -52,9 +103,13 @@ extension TripEvent: DatabaseRepresentation {
         let rep: [String: Any] = [
             "title": self.title,
             "start": self.start,
-            "duration": self.duration
+            "duration": self.duration,
+            
+            "placeID": self.placeID,
+            "placeName": self.placeName,
+            "placeLat": self.placeLat,
+            "placeLon": self.placeLon
         ]
         return rep
     }
-    
 }
