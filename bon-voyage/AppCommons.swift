bd1d6a43238207a58569f9ee92358ed1a45e16a3
harsilspatel <harsilspatel@gmail.com>
2019-06-15 17:45:55 +1000

    add app commons

---
+++ bon-voyage/AppCommons.swift
@@ -0,0 +1,23 @@
+//
+//  AppCommons.swift
+//  bon-voyage
+//
+//  Created by Harsil Patel on 15/6/19.
+//  Copyright © 2019 Harsil Patel. All rights reserved.
+//
+
+import Firebase
+import Foundation
+import GooglePlaces
+
+class AppCommons {
+    static let sharedInstance = AppCommons()
+    
+    let database = Firestore.firestore()
+    let placesClient = GMSPlacesClient.shared()
+    
+    private init() {
+//        database =
+//        placesClient = GMSPlacesClient.shared()
+    }
+}
